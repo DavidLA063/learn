2a2d9fb86f38efbe6b39d3022b446e2c72e28f73
David <david063@mail.ru>
2022-08-17 16:26:02 +0300

    add function like user

---
+++ ex2_likeuser/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine ();
+
+if (username.ToLower() == "валера") {
+    Console.WriteLine("Ура это Валера !");
+}
+else {
+    Console.Write("Привет ");
+    Console.WriteLine(username);
+}
