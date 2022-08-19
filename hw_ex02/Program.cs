bf96f07f2b63788adb4fbee1b6e60b9a6ef254bf
David <david063@mail.ru>
2022-08-19 12:38:44 +0300

    add home work ex 02 v2.

--- hw_ex02/Program.cs
+++ hw_ex02/Program.cs
@@ -1,17 +1,17 @@
 ﻿/* Напишите программу, которая на вход принимает два числа и выдаёт, 
 какое число большее, а какое меньшее.*/
+Console.WriteLine ("Введите два числа: ");
 
-
-int numberA = -5;
-int numberB = -9;
+int numberA = int.Parse (Console.ReadLine () );
+int numberB = int.Parse (Console.ReadLine () );
 
 
 if ( numberA > numberB ) {
-    Console.Write ("Максимальное число ");
-    Console.WriteLine (numberA);
+    Console.WriteLine ($"Большее число: {numberA} ");
+    Console.WriteLine ($"Меньшее число: {numberB} ");
     }
     else
     {
-        Console.Write ("Максимальное число ");
-        Console.WriteLine (numberB);
+        Console.WriteLine ($"Большее число: {numberB}");
+        Console.WriteLine ($"Меньшее число: {numberA}");
     }
