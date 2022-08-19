74cdbfbd7d75d1c95c044f0c38eca91e2ec49f65
David <david063@mail.ru>
2022-08-19 10:02:27 +0300

    add home work ex4

---
+++ hw_ex04/Program.cs
@@ -0,0 +1,16 @@
+﻿/*Напишите программу, которая принимает на вход три числа 
+и выдаёт максимальное из этих чисел.*/
+
+int numberA = 8;
+int numberB = 10;
+int numberC = 2;
+
+int maxNumber = numberA;
+
+if ( numberA > maxNumber ) maxNumber = numberA;
+if ( numberB > maxNumber ) maxNumber = numberB;
+if ( numberC > maxNumber ) maxNumber = numberC;
+
+Console.Write (maxNumber);
+
+
