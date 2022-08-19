c6d453246c2d40fa9f5e232806f4c78dea7ec4e0
David <david063@mail.ru>
2022-08-19 12:18:47 +0300

    add home work ex8 v2

--- hw_ex08/Program.cs
+++ hw_ex08/Program.cs
@@ -1,9 +1,8 @@
 ﻿/*  Напишите программу, которая на вход принимает число (N),
  а на выходе показывает все чётные числа от 1 до N.   5 -> 2, 4  */
 
-
-
-int numA = 9;
+Console.WriteLine ("Введите число :");
+int numA = int.Parse( Console.ReadLine () );
 
 for (int i = 0; i <= numA; i=i+2)
 {
